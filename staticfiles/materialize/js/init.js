9c04c5283d2300edca989e550bedb7595a45640f
shyamkumaryadav <shyamkumaryadav2003@gmail.com>
2020-12-10 00:01:10 +0530

    RM staticfile

--- staticfiles/materialize/js/init.js
+++
@@ -1,9 +0,0 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-    $(".dropdown-trigger").dropdown();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
